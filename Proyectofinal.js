7f77f5e8025d11374876e244f71f33381a3963c6
Nicolas Di Scala <ndiscala56@gmail.com>
2021-10-16 12:40:39 -0300

    firt commit

---
+++ Proyectofinal.js
@@ -0,0 +1,31 @@
+
+function planta(nombre,tamaño,color,precio) {
+    this.nombre=nombre;
+    this.tamaño=tamaño;
+    this.color=color;
+    this.precio=precio;
+}
+
+
+const planta1 = new planta ("Cactus","pequeño","verde",100);
+const planta2 = new planta ("Suculenta","mediano","verde",200)
+const planta3 = new planta ("Olivo","mediano","verde claro",300);
+const planta4 = new planta ("Naranjo","grande","Naranja",500);
+const planta5 = new planta ("Limonero","grande","Amarillo",700);
+const planta6 = new planta
+prompt("Ingrese planta a elección")
+prompt("Ingrese tamaño")
+prompt("Ingrese color")
+prompt("Ingrese precio máximo a pagar");
+
+
+
+
+
+
+
+
+
+
+
+
